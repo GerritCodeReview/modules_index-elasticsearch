aa01a040c49032481e3d49720dbb2e1ba5a9d15c
Matthias Sohn <matthias.sohn@sap.com>
2024-10-16 13:00:12 +0200

    Fix version check regexes and allow any 7.x and 8.x versions
    The regex patterns were glob expressions but were used to create Java
    regular expressions which is wrong. Fix the patterns.
    
    Relax the rules to allow any ElasticSearch 7.x and 8.x server version
    since the low-level REST client documentation says [1]:
    
    "There is no relation between the client version and the Elasticsearch
    version that the client can communicate with. The low-level REST
    client is compatible with all Elasticsearch versions."
    
    Also allow non-releases like alpha, beta or release candidates.
    
    [1] https://www.elastic.co/guide/en/elasticsearch/client/java-api-client/current/java-rest-low-usage-maven.html
    
    Change-Id: Ia14678200a9b92bbe195b176321e34e2bba7b92d

--- src/test/java/com/google/gerrit/elasticsearch/ElasticV7QueryAccountsTest.java
+++ src/test/java/com/google/gerrit/elasticsearch/ElasticV7QueryAccountsTest.java
@@ -19,6 +19,6 @@ import org.junit.BeforeClass;
 public class ElasticV7QueryAccountsTest extends ElasticAbstractQueryAccountsTest {
   @BeforeClass
   public static void startIndexService() {
-    startIndexService(ElasticVersion.V7_17);
+    startIndexService(ElasticVersion.V7);
   }
 }
